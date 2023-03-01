32dd3cff764012736e0081f17bfca8430a38e7a1
CarloDubini <45142126+CarloDubini@users.noreply.github.com>
2023-03-01 17:35:39 +0100

    Create Nodo.h

---
+++ Práctica  1  IC/Nodo.h
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<set>
+#include<vector>
+
+using namespace std;
+
+
+struct tNodo {
+	int x = 0, y = 0; //posición del nodo
+	pair<int, int> padre; //para saber quien es su padre. Te acuerdas de las flechitas en los ejercicios de IC? pues eso
+	vector<pair<int, int>> hijos; //son los hijos del nodo.
+	int h = 0, g = 0;
+	int f = 0;
+	string tipo = "libre"; //{libre,peligroso,bloqueado}
+	bool penalizacion = false; 
+};
+
+/*
+reglas:
+- Si está en la lista abierta o cerrada, el nodo tiene un padre.
+
+
+*/
